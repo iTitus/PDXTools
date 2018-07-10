c89bb3ed1f205e65abf0285df50bcc58aceab8da
iTitus <iTitus@users.noreply.github.com>
2018-07-10 19:36:37 +0200

    Add missing field

--- src/main/java/io/github/ititus/pdx/stellaris/user/save/LeaderDesign.java
+++ src/main/java/io/github/ititus/pdx/stellaris/user/save/LeaderDesign.java
@@ -5,7 +5,7 @@ import io.github.ititus.pdx.pdxscript.PdxScriptObject;
 public class LeaderDesign {
 
     private final int texture, hair, clothes;
-    private final String gender, name, portrait;
+    private final String gender, name, portrait, rulerTitle;
 
     public LeaderDesign(PdxScriptObject o) {
         this.gender = o.getString("gender");
@@ -14,15 +14,17 @@ public class LeaderDesign {
         this.texture = o.getInt("texture");
         this.hair = o.getInt("hair");
         this.clothes = o.getInt("clothes");
+        this.rulerTitle = o.getString("ruler_title");
     }
 
-    public LeaderDesign(int texture, int hair, int clothes, String gender, String name, String portrait) {
+    public LeaderDesign(int texture, int hair, int clothes, String gender, String name, String portrait, String rulerTitle) {
         this.texture = texture;
         this.hair = hair;
         this.clothes = clothes;
         this.gender = gender;
         this.name = name;
         this.portrait = portrait;
+        this.rulerTitle = rulerTitle;
     }
 
     public int getTexture() {
@@ -47,5 +49,9 @@ public class LeaderDesign {
 
     public String getPortrait() {
         return portrait;
+    }
+
+    public String getRulerTitle() {
+        return rulerTitle;
     }
 }
