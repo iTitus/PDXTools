12f1dbc26e37d1996b977320cc6d2543f9d586cb
iTitus <iTitus@users.noreply.github.com>
2021-06-18 09:08:13 +0200

    update assertj

--- stellaris-lwjgl-viewer/src/main/java/module-info.java
+++ stellaris-lwjgl-viewer/src/main/java/module-info.java
@@ -5,13 +5,18 @@ module io.github.ititus.stellaris.lwjgl.viewer {
     requires io.github.ititus.pdx;
     requires org.eclipse.collections.api;
     requires org.eclipse.collections.impl;
-
     requires org.lwjgl;
     requires org.lwjgl.natives;
+    requires org.lwjgl.assimp;
+    requires org.lwjgl.assimp.natives;
     requires org.lwjgl.glfw;
     requires org.lwjgl.glfw.natives;
+    requires org.lwjgl.openal;
+    requires org.lwjgl.openal.natives;
     requires org.lwjgl.opengl;
     requires org.lwjgl.opengl.natives;
+    requires org.lwjgl.stb;
+    requires org.lwjgl.stb.natives;
     requires org.joml;
 
     exports io.github.ititus.stellaris.lwjgl.viewer;
